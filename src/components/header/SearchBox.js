665e473bb96cb6cddb0c5dc7131d8831deee4865
Ricardo-Paul <ricardopaul459@gmail.com>
2020-10-24 19:36:29 -0400

    fake anchorEl to popper

--- src/components/header/SearchBox.js
+++ src/components/header/SearchBox.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-import { IconButton, InputBase } from '@material-ui/core';
+import { Divider, IconButton, InputBase } from '@material-ui/core';
 import { ArrowBack } from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
 import { SearchBoxContext } from '../GlobalState';
 import SuggestSearch from '../apis/SuggestSearch';
 import YoutubeSearch from '../apis/YoutubeSearch';
+
+// suggestionResult
+import { List, ListItem, ListItemIcon, ListItemText, Popper } from '@material-ui/core'
+import { Search } from '@material-ui/icons';
+
 
 const styles = {
     arrow: {
@@ -16,34 +21,27 @@ const styles = {
  const SearchBox = (props) => {
      const { classes } = props;
      const { isSearchOpen, setSearch } = React.useContext(SearchBoxContext)
-
+     const [ suggestionResults, setSuggestionResult ] = React.useState([]);
 
     const [state, setState] = React.useState({
         searchQuery: ''
     })
+
+    async function getSuggestions(value){
+        const suggestionResult = await SuggestSearch.get("",{params:{ q:value}})
+        setSuggestionResult(suggestionResult.data[1])
+    }
   
+    async function searchOnYoutube(selectedValue){
+        const searchResult = await YoutubeSearch.get("/search",{params:{q:selectedValue}})
+        return searchResult.data;
+    }
 
      const onType = async (e) => {
-         console.log(process.env.API_KEY)
         const typedValue = e.target.value;
         setState({...state, searchQuery: typedValue})
-
-        const suggestionResult = await SuggestSearch.get("",{
-            params:{q: typedValue}
-        })
-
-        console.log(suggestionResult.data[1], typeof(suggestionResult));
-
-        // try{
-        //     const result = await YoutubeSearch.get("/search", {
-        //         params:{
-        //             q: typedValue
-        //         }
-        //     })
-        //     console.log(result.data)
-        // } catch (error){
-        //     console.log(error)
-        // }
+        getSuggestions(typedValue)
+        console.log(suggestionResults)
     }
 
     return(
@@ -66,6 +64,17 @@ const styles = {
             onChange={onType}
             />
         </form>
+
+        <Popper style={{ width: "100&" }} open={true} placement="bottom" anchorEl={document.getElementById("root")}>
+        <List style={{ width: "100%"}}>
+            <ListItem>
+                <ListItemIcon>
+                   <Search />
+                </ListItemIcon>
+                <ListItemText primary="Eminem" />
+            </ListItem>
+        </List>
+        </Popper>
         </>
     )
 }
